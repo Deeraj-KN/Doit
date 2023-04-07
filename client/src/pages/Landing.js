485bca238932ba5713b1f5a38be7bfa3cabd2198
Deeraj-KN <deeraj.kn2000@gmail.com>
2023-04-07 13:35:18 +0530

    asssets added

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,20 @@
+import logo from '../assets/images/logo.png'
+import main from '../assets/images/main.svg'
+
+const Landing = () => {
+  return (
+    <div>
+      <main>
+        <nav>
+            <img src={logo} alt='doit' className='logo'/>
+
+        </nav>
+        <div className='container'>
+            
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default Landing
